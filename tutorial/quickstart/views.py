d1712ee07d7f98dd12116bdc0aa6ce54c1300e52
toni_sheen <newtonmigosi@gmail.com>
2019-01-15 12:27:46 +0200

    implemented viewsets

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
 
-# Create your views here.
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
